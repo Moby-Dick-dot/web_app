039bb293d9f1c5e422e636a1fd2a95c073d892a1
yyc <1486541151@qq.com>
2024-09-17 11:03:31 +0800

    init

---
+++ dao/mysql/mysql.go
@@ -0,0 +1,38 @@
+package mysql
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/spf13/viper"
+)
+
+// 定义一个全局对象db
+var db *sql.DB
+
+// Init 定义一个初始化数据库的函数
+func Init() (err error) {
+	// DSN:Data Source Name
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
+		viper.GetString("mysql.user"),
+		viper.GetString("mysql.password"),
+		viper.GetString("mysql.host"),
+		viper.GetInt("mysql.port"),
+		viper.GetString("mysql.dbname"),
+	)
+	db, err := sqlx.Connect("mysql", dsn)
+	if err != nil {
+		fmt.Printf("connect DB failed,err:%v\n", err)
+		return
+	}
+
+	db.SetMaxOpenConns(viper.GetInt("mysql.max_open_conns"))
+	db.SetMaxIdleConns(viper.GetInt("mysql.max_idle_conns"))
+	return
+}
+
+func Close() {
+	_ = db.Close()
+}
